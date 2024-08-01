4bae90d8cc324518fb9b1bb8777368cad5952a2d
halit uzan <halit.uzan@gmail.com>
2024-08-02 00:18:23 +0300

    updates

--- utils/Network.tsx
+++ utils/Network.tsx
@@ -22,7 +22,7 @@ const network = class NETWORK {
 
   constructor(axios: { create: (arg0: { baseURL: string }) => any }) {
     this.network = axios.create({
-      baseURL: "http://localhost:3000/api",
+      baseURL: "https://halituzan.github.io/api",
     });
   }
 
