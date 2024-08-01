ec91783a54c8d09281ea203dff995d28d85d3c9f
halit uzan <halit.uzan@gmail.com>
2024-08-02 00:15:23 +0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,8 @@
 
 
 const nextConfig = {
-    // output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    images: { unoptimized: true }
+    // reactStrictMode: true,
+    // images: { unoptimized: true }
 };
 
 export default nextConfig;
